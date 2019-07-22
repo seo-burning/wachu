4e8c99f986b9d91c69299ea35d05bdfe38624b1c
SeongukSeo <su.seo@burningb.com>
2019-07-22 20:53:37 +0900

    [add] add user id info in serializer

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -31,7 +31,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ('email', 'password', 'name')
+        fields = ('id', 'email', 'password', 'name')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 6}}
 
     def create(self, validated_data):
