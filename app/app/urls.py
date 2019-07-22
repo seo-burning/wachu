43ea4c03a3b50e818e44805185208f21f11b8e16
SeongukSeo <su.seo@burningb.com>
2019-07-22 21:17:10 +0900

    [del] remove drf_yasg setup

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,34 +19,34 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic import TemplateView
-from rest_framework import permissions
+# from rest_framework import permissions
 
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Snippets API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Snippets API",
+#         default_version='v1',
+#         description="Test description",
+#         terms_of_service="https://www.google.com/policies/terms/",
+#         contact=openapi.Contact(email="contact@snippets.local"),
+#         license=openapi.License(name="BSD License"),
+#     ),
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
 
 
 urlpatterns = [
-    url(r'^swagger(?P<format>\.json|\.yaml)$',
-        schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger',
-                                           cache_timeout=0),
-        name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc',
-                                         cache_timeout=0),
-        name='schema-redoc'),
+    # url(r'^swagger(?P<format>\.json|\.yaml)$',
+    #     schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # url(r'^swagger/$', schema_view.with_ui('swagger',
+    #                                        cache_timeout=0),
+    #     name='schema-swagger-ui'),
+    # url(r'^redoc/$', schema_view.with_ui('redoc',
+    #                                      cache_timeout=0),
+    #     name='schema-redoc'),
     url(r'^accounts/', include('allauth.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
     path('privacy-policy/',
