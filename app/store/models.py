d0dd66334c8ffe2d590586b2955168c91b24c23a
SeongukSeo <su.seo@burningb.com>
2019-07-29 17:16:34 +0900

    [fix] flake8 fixed

--- app/store/models.py
+++ app/store/models.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.utils.safestring import mark_safe
 
 from django.utils.translation import ugettext_lazy as _
-from product.models import Product
 
 
 class TimeStampedModel(models.Model):
