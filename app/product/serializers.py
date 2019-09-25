950dcdf9e630b9f371846e47f325a2381748b249
SeongukSeo <su.seo@burningb.com>
2019-09-25 19:37:43 +0900

    [fix] add store json info in product

--- app/product/serializers.py
+++ app/product/serializers.py
@@ -1,27 +1,27 @@
 from rest_framework import serializers
 
 from product import models
-from publish.serializers import PostImageSerializer
+from publish.serializers import PostImageSerializer, StorePostSerializer
 from store.models import StorePost
 
 
-class StorePostSerializer(serializers.ModelSerializer):
-    post_image_set = PostImageSerializer(read_only=True, many=True)
-    store = serializers.PrimaryKeyRelatedField(read_only=True, many=False)
+# class StorePostSerializer(serializers.ModelSerializer):
+#     post_image_set = PostImageSerializer(read_only=True, many=True)
+#     store = serializers.PrimaryKeyRelatedField(read_only=True, many=False)
 
-    class Meta:
-        model = StorePost
-        fields = ('pk', 'post_url', 'post_type', 'post_thumb_image',
-                  'video_source',
-                  'post_description', 'post_image_set', 'store')
+#     class Meta:
+#         model = StorePost
+#         fields = ('pk', 'post_url', 'post_type', 'post_thumb_image',
+#                   'video_source',
+#                   'post_description', 'post_image_set', 'store')
 
-    @staticmethod
-    def setup_eager_loading(queryset):
-        """ Perform necessary eager loading of data. """
-        # select_related for "to-one" relationships
-        queryset = queryset.select_related('store')
-        queryset = queryset.prefetch_related('post_image_set')
-        return queryset
+#     @staticmethod
+#     def setup_eager_loading(queryset):
+#         """ Perform necessary eager loading of data. """
+#         # select_related for "to-one" relationships
+#         queryset = queryset.select_related('store')
+#         queryset = queryset.prefetch_related('post_image_set')
+#         return queryset
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -37,8 +37,16 @@ class ProductSerializer(serializers.ModelSerializer):
     def setup_eager_loading(queryset):
         """ Perform necessary eager loading of data. """
         # select_related for "to-one" relationships
-        queryset = queryset.select_related('post', 'sub_category')
+        queryset = queryset.select_related(
+            'post',
+            'sub_category',
+            'post__store',
+            'post__store__age',
+            'post__store__primary_style',
+            'post__store__secondary_style')
         queryset = queryset.prefetch_related('tag',
                                              'post__post_image_set',
-                                             'color',)
+                                             'color',
+                                             'post__store__category',
+                                             )
         return queryset
