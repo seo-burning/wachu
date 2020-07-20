fe7ff54ea88ae7f23bf7cd1356ecd27716cbc1bc
SeongukSeo <su.seo@burningb.com>
2020-07-20 15:16:36 +0700

    Fix time

--- app/crawling/daily.py
+++ app/crawling/daily.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 
 from shopee_c import update_shopee, validate_shopee
 from point_calculating import calculate_store_review_rating, \
@@ -16,10 +17,11 @@ def calculating():
 
 if __name__ == '__main__':
     date = datetime.datetime.now().strftime('%Y-%m-%d %H:00')
-    time = int(datetime.datetime.now().strftime('%H'))
+    at_time = int(datetime.datetime.now().strftime('%H'))
     start_time = time.time()
+    slack_notify("--- %s seconds ---" % (time.time() - start_time))
 
-    if time > 18:  # 25시에 진행되는 크롤링 (01:00)        # 쇼피 뒤 절반 생성 / 홈페이지 업데이트
+    if at_time > 18:  # 25시에 진행되는 크롤링 (01:00)        # 쇼피 뒤 절반 생성 / 홈페이지 업데이트
         slack_notify('*update data @' + date+'*')
         # 쇼피 앞 절반 생성 / 홈페이지 생성
         slack_notify('*update homepage @' + date+'*')
@@ -30,7 +32,7 @@ if __name__ == '__main__':
         calculating()
         preview_image_update()
         slack_notify('*update data @' + date+'*' + 'done')
-    elif time > 22:  # 29시에 진행되는 크롤링 (05:00)  상품 검증
+    elif at_time > 22:  # 29시에 진행되는 크롤링 (05:00)  상품 검증
         slack_notify('*validate data @' + date+'*')
         slack_notify('*validate homepage @' + date+'*')
         validate_homepage()
@@ -40,7 +42,7 @@ if __name__ == '__main__':
         calculating()
         preview_image_update()
         slack_notify('*update data @' + date+'*' + 'done')
-    elif time > 7:  # 14시 진행되는 크롤링         # 홈페이지 신규 상품 생성 / 쇼피 앞 절반 생성
+    elif at_time > 7:  # 14시 진행되는 크롤링         # 홈페이지 신규 상품 생성 / 쇼피 앞 절반 생성
         slack_notify('*update data @' + date+'*')
         slack_notify('*update homepage @' + date+'*')
         update_homepage()
@@ -50,5 +52,3 @@ if __name__ == '__main__':
         calculating()
         preview_image_update()
         slack_notify('*update data @' + date+'*' + 'done')
-
-    slack_notify("--- %s seconds ---" % (time.time() - start_time))
