bba566c50d4285e063731c8f10d8ca93863794aa
SeongukSeo <su.seo@burningb.com>
2020-08-04 15:12:35 +0700

    Add requirements

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic import TemplateView
+
+
 # from rest_framework import permissions
 
 # from drf_yasg.views import get_schema_view
